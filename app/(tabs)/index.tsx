fcf1314493b4db00891da553886fba0393ab20f6
DiluDevX <dilumchandupa@gmail.com>
2025-06-09 22:01:52 +0530

    feat: update app name and slug, add image saving functionality, and include dom-to-image library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,11 +5,13 @@ import EmojiPicker from "@/components/EmojiPicker";
 import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
+import domtoimage from "dom-to-image";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { useRef, useState } from "react";
-import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import { ImageSourcePropType, Platform, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("@/assets/images/icon.png");
 
@@ -54,7 +56,39 @@ export default function Index() {
     console.log("Add sticker button pressed");
   };
 
-  const onSaveImageAsync = async () => {};
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== "web") {
+      try {
+        const localUrl = await captureRef(imageRef, {
+          width: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUrl);
+
+        if (localUrl) {
+          alert("Image saved successfully!");
+        }
+      } catch (error) {
+        console.error("Error saving image:", error);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 1,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "PhotoSticky.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (error) {
+        console.error("Error saving image:", error);
+      }
+    }
+  };
 
   return (
     <GestureHandlerRootView style={styles.container}>
